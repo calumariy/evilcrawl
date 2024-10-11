428779cd30775949c6bcaf5c626f20b715fba560
Callum <callumreilly122@gmail.com>
2024-10-11 13:06:24 +1100

    Finished creating basic subdomain enumeration

--- main.go
+++ main.go
@@ -4,23 +4,40 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	domainPtr := flag.String("d", "", "the ip of target")
 	wordlistPtr := flag.String("w", "", "the wordlist for subdomain enumeration")
+	customSubdomainsPtr := flag.String("c", "", "the wordlist of subdomains you have found")
+	outFilePtr := flag.String("o", "", "output file of program")
 	flag.Parse()
+
 	domain := *domainPtr
 	wordlist := *wordlistPtr
+	customSubdomains := *customSubdomainsPtr
+	outFileName := *outFilePtr
+
 	if domain == "" {
-		fmt.Fprintln(os.Stderr, "error: please enter a domain with -d")
+		fmt.Fprintln(os.Stderr, "[x] ERROR: please specify a domain or ip address with -d")
 		os.Exit(1)
 	}
 
-	subdomains, err := compileSubdomains(domain, wordlist)
+	outFile := os.Stdout
+	if outFileName == "" {
+		fmt.Fprintln(os.Stderr, "[!] No out file specified - output will be set to stdout")
+	} else {
+		var err error
+		outFile, err = os.OpenFile(outFileName, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+		handleErr(err)
+	}
+
+	subdomains, err := compileSubdomains(domain, wordlist, customSubdomains)
 	handleErr(err)
 	amntSubdomains := len(subdomains)
-	for i := 0; i < amntSubdomains; i++ {
-		fmt.Println(subdomains[i])
+	for _, subdomain := range subdomains {
+		fmt.Fprintln(outFile, subdomain)
 	}
+	fmt.Fprintln(os.Stderr, "[.] found "+strconv.Itoa(amntSubdomains)+" subdomains! Launching workers...")
 }
--- subdomains.go
+++ subdomains.go
@@ -8,27 +8,66 @@ import (
 	"strings"
 )
 
-func compileSubdomains(mainDomain string, wordlist string) ([]string, error) {
+func compileSubdomains(mainDomain string, wordlist string, customSubdomainsFile string) ([]string, error) {
 	// Check that domain exists
 	_, err := http.Get(mainDomain)
 	handleErr(err)
 	passiveSubdomains, err := passiveDNSRecon(mainDomain)
+
 	var activeSubdomains []string
 	if wordlist == "" {
-		fmt.Fprintln(os.Stderr, "no wordlist detected, skipping brute force")
+		fmt.Fprintln(os.Stderr, "[!] no wordlist detected, skipping brute force")
 	} else {
 		activeSubdomains, err = activeDNSRecon(mainDomain, wordlist)
 		handleErr(err)
 	}
 
-	joinSubdomains(passiveSubdomains, activeSubdomains)
+	passiveAndActiveDomains, err := joinSubdomains(passiveSubdomains, activeSubdomains)
+	handleErr(err)
 
-	subdomains := []string{mainDomain}
+	customSubdomains := []string{}
+	customSubdomainList, err := os.Open(customSubdomainsFile)
+	handleErr(err)
+
+	subdomains := []string{}
+
+	if customSubdomainsFile != "" {
+		customSubdomainScanner := bufio.NewScanner(customSubdomainList)
+		customSubdomainScanner.Split(bufio.ScanLines)
+		for customSubdomainScanner.Scan() {
+			customSubdomain := customSubdomainScanner.Text()
+			if !strings.Contains(customSubdomain, "http://") {
+				customSubdomain = "http://" + customSubdomain
+			}
+			_, err := http.Get(customSubdomain)
+			if err != nil {
+				continue
+			}
+			customSubdomains = append(customSubdomains, customSubdomain)
+		}
+		subdomains, err = joinSubdomains(passiveAndActiveDomains, customSubdomains)
+		handleErr(err)
+	} else {
+		fmt.Fprintln(os.Stderr, "[!] no custom subdomains supplied")
+	}
+
+	foundDomain := false
+	for _, domainName := range subdomains {
+		if domainName == mainDomain {
+			foundDomain = true
+			break
+		}
+	}
+
+	if !foundDomain {
+		subdomains = append(subdomains, mainDomain)
+	}
 
 	return subdomains, nil
 }
 
 func passiveDNSRecon(mainDomain string) ([]string, error) {
+	fmt.Fprintln(os.Stderr, "[!] Passive dns recon not supported yet")
 	return []string{mainDomain}, nil
 }
 
@@ -61,9 +100,25 @@ func activeDNSRecon(mainDomain string, wordlistPath string) ([]string, error) {
 		subdomains = append(subdomains, subdomain)
 	}
 
-	return []string{mainDomain}, nil
+	return subdomains, nil
 }
 
 func joinSubdomains(subdomains1 []string, subdomains2 []string) ([]string, error) {
-	return []string{}, nil
+	m := make(map[string]bool)
+	compiledSubdomains := []string{}
+
+	for _, domain1 := range subdomains1 {
+		m[domain1] = true
+		compiledSubdomains = append(compiledSubdomains, domain1)
+	}
+
+	for _, domain2 := range subdomains2 {
+		_, hasDomain := m[domain2]
+		if !hasDomain {
+			compiledSubdomains = append(compiledSubdomains, domain2)
+			m[domain2] = true
+		}
+	}
+
+	return compiledSubdomains, nil
 }
