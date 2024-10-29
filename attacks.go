66e10ee926a02defadec020ca2b83fd7ca34010e
Callum Reilly <z5592665@ad.unsw.edu.au>
2024-10-29 21:09:22 +1100

    start of chromedp cookies

--- attacks.go
+++ attacks.go
@@ -3,14 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
+	"time"
+
+	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
 	"golang.org/x/net/html"
-	"log"
-	"time"
 )
 
 func attackInput(baseSub string, baseDomain string, n *html.Node) {
-	fmt.Println("[!] Input found at " + baseDomain)
 	for _, attr := range n.Attr {
 		if attr.Key == "type" && attr.Val == "password" && !isAuthorised {
 			fmt.Println("[!] found password field on: " + baseDomain + "\nWant to use authorisation? Use the -a flag!")
@@ -24,21 +25,49 @@ func attackInput(baseSub string, baseDomain string, n *html.Node) {
 	defer cancel()
 
 	// create a timeout
-	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+	ctx, cancel = context.WithTimeout(ctx, time.Second)
 	defer cancel()
 
 	// navigate to a page, wait for an element, click
-	var example string
 	err := chromedp.Run(ctx,
+		setCookie(baseDomain),
+		// Navigate to domain
 		chromedp.Navigate(baseDomain),
-		// wait for footer element is visible (ie, page is loaded)
-		chromedp.WaitVisible(`body > footer`),
+		// Load webpage
+		chromedp.WaitReady("body", chromedp.ByQuery),
 		// find and click "Example" link
-		chromedp.Click(`#example-After`, chromedp.NodeVisible),
-		// retrieve the text of the textarea
-		chromedp.Value(`#example-After textarea`, &example),
+		chromedp.Click(`button[type="submit"]`),
 	)
 	if err != nil {
-		log.Fatal(err)
+		if err != context.DeadlineExceeded {
+			handleErr(err)
+		}
+		return
 	}
+
+	// Button found, try to do more
+	fmt.Println("[!] submit buuton found at ")
+}
+
+func setCookie(domain string) chromedp.ActionFunc {
+	return chromedp.ActionFunc(func(ctx context.Context) error {
+
+		if auth == "" {
+			return nil
+		}
+
+		domainPath := strings.Split(domain, "/")
+
+		mainDomain := domainPath[2]
+		mainDomainArr := strings.Split(mainDomain, ".")
+		cookieDomain := "." + mainDomainArr[len(mainDomainArr)-2] + "." + mainDomainArr[len(mainDomainArr)-1]
+
+		cookieStr := strings.Split(auth, ":")
+
+		err := network.SetCookie(cookieStr[0], cookieStr[1]).WithDomain(cookieDomain).Do(ctx)
+		if err != nil {
+			fmt.Errorf("error: %w")
+		}
+		return err
+	})
 }
